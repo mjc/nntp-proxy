f427a453787d5c151dd536713616126828399c52
Mika Cohen <mjc@kernel.org>
2026-05-22 14:32:53 -0600

    Launch local TUI after startup setup

--- src/bin/nntp-proxy.rs
+++ src/bin/nntp-proxy.rs
@@ -76,28 +76,16 @@ async fn run_proxy(
     let proxy = build_proxy(config, launch.routing_mode, launch.metrics_store).await?;
 
     let (shutdown_tx, shutdown_rx) = mpsc::channel::<()>(1);
-    let (tui_handle, tui_shutdown_tx) =
-        launch_tui(ui_mode, &proxy, log_buffer.clone(), shutdown_tx.clone());
 
     if let Some(path) = launch.availability_path.as_ref() {
         let _ = runtime::load_availability_from_disk(proxy.cache(), path);
     }
 
-    let listener =
-        match runtime::bind_listener(&launch.host, launch.port, launch.routing_mode).await {
-            Ok(listener) => listener,
-            Err(err) => {
-                shutdown_startup_tui(tui_handle, tui_shutdown_tx).await;
-                return Err(err);
-            }
-        };
+    let listener = runtime::bind_listener(&launch.host, launch.port, launch.routing_mode).await?;
 
     // Prewarm connections BEFORE accepting clients (must complete first to avoid exceeding limits)
     info!("Prewarming connection pools...");
-    if let Err(err) = proxy.prewarm_connections().await {
-        shutdown_startup_tui(tui_handle, tui_shutdown_tx).await;
-        return Err(err);
-    }
+    proxy.prewarm_connections().await?;
     info!("Connection pools ready, accepting clients");
 
     runtime::spawn_stats_flusher(proxy.connection_stats());
@@ -114,13 +102,9 @@ async fn run_proxy(
     runtime::spawn_tokio_runtime_metrics_logger();
 
     let (dashboard_handle, dashboard_shutdown_tx) =
-        match launch_dashboard_publisher(args.common.tui_listen, &proxy, log_buffer).await {
-            Ok(handles) => handles,
-            Err(err) => {
-                shutdown_startup_tui(tui_handle, tui_shutdown_tx).await;
-                return Err(err);
-            }
-        };
+        launch_dashboard_publisher(args.common.tui_listen, &proxy, log_buffer.clone()).await?;
+    let (tui_handle, tui_shutdown_tx) =
+        launch_tui(ui_mode, &proxy, log_buffer, shutdown_tx.clone());
     let error_tui_shutdown_tx = tui_shutdown_tx.clone();
     let error_dashboard_shutdown_tx = dashboard_shutdown_tx.clone();
     spawn_signal_forwarder(shutdown_tx, tui_shutdown_tx, dashboard_shutdown_tx);
@@ -155,23 +139,6 @@ async fn run_proxy(
     }
 
     accept_result
-}
-
-async fn shutdown_startup_tui(
-    tui_handle: Option<TuiHandle>,
-    tui_shutdown_tx: Option<mpsc::Sender<()>>,
-) {
-    if let Some(tx) = tui_shutdown_tx {
-        let _ = tx.send(()).await;
-    }
-    if let Some(handle) = tui_handle
-        && let Err(err) = handle.await
-    {
-        warn!(
-            "TUI task failed while shutting down after startup error: {}",
-            err
-        );
-    }
 }
 
 async fn build_proxy(
